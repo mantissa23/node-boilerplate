cf8574d17b1a115040c0d50d9d02c45ef1313ff1
Valerii Iatsko <viatsko@viatsko.me>
2018-02-16 19:19:00 +0100

    Added morgan

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('common'));
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
